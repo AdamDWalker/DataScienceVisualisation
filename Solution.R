67997c253bba0b40be1983e9136b0565c343c597
AdamDWalker <adamwalker03@googlemail.com>
2017-05-04 09:22:28 +0100

    Added new plotting stuff

--- Solution.R
+++ Solution.R
@@ -2,17 +2,20 @@ df <- read.csv("Bike_Rental_Dataset.csv", header=TRUE, sep=",")
 View(df)
 
 install.packages("ggplot2")
+install.packages("lattice")
 install.packages("lubridate")
 install.packages("scales")
 install.packages("plyr")
 install.packages("readr")
 
 library(ggplot2)
+library(lattice)
 library(lubridate)
 library(scales)
 library(plyr)
 library(readr)
 
+xyplot(cnt ~ hum|weather, data=df)
 
 df$season  <- factor(df$season, labels = c("Spring", "Summer", "Fall", "Winter"))
 df$weather <- factor(df$weather, labels = c("Good", "Normal", "Bad", "Very Bad"))
@@ -23,11 +26,14 @@ df$Weekday <- wday(ymd_hms(df$datetime), label=TRUE)
 weather_summary <- ddply(df,.(weather,hour),
                          summarise, cnt = mean(cnt))
 
-temp_summary <- ddply(df,.(weather,temp),
+temp_summary <- ddply(df,.(weather,atemp),
                          summarise, cnt = mean(cnt))
 
 season_summary <- ddply(df,.(season,hour),
                       summarise, cnt = mean(cnt))
+
+season_temp_summary <- ddply(df,.(season,temp),
+                        summarise, cnt = mean(cnt))
 
 ## ===================== Generates a plot for hour/weather ===================== ##
 ggplot(df, aes(x = hour, y = cnt, colour = weather)) +
@@ -44,13 +50,13 @@ ggplot(df, aes(x = hour, y = cnt, colour = weather)) +
   panel.background = element_blank())
 
 ## ===================== Generates a plot for temp/weather ===================== ##
-ggplot(df, aes(x = temp, y = cnt, colour = weather)) +
+ggplot(df, aes(x = atemp, y = cnt, colour = weather)) +
   geom_point(data = temp_summary, aes(group = weather)) +
   geom_line(data = temp_summary, aes(group = weather)) +
-  scale_x_continuous("Temperature (Normalised)") +
+  scale_x_continuous("Feeling Temperature (Normalised)") +
   scale_y_continuous("Count") +
   theme_minimal() +
-  ggtitle("Rentals v temperature per weather condition\n") + 
+  ggtitle("Count and feeling temperature per weather condition\n") + 
   theme(plot.title=element_text(size=18),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -65,6 +71,20 @@ ggplot(df, aes(x = hour, y = cnt, colour = season)) +
   scale_y_continuous("Count") +
   theme_minimal() +
   ggtitle("Average hourly rentals per season\n") + 
+  theme(plot.title=element_text(size=18),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(color = 'black'),
+        panel.background = element_blank())
+
+## ===================== Generates a plot for temp/season ===================== ##
+ggplot(df, aes(x = temp, y = cnt, colour = season)) +
+  geom_point(data = season_temp_summary, aes(group = season)) +
+  geom_line(data = season_temp_summary, aes(group = season)) +
+  scale_x_continuous("Temp") +
+  scale_y_continuous("Count") +
+  theme_minimal() +
+  ggtitle("Rental count/temperature per season\n") + 
   theme(plot.title=element_text(size=18),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
